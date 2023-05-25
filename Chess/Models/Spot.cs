e463b41dd48786a94a79921815608ab74a0cec60
Tony <adaguayza@gmail.com>
2023-05-25 10:39:19 -0500

    first commit

---
+++ Chess/Models/Spot.cs
@@ -0,0 +1,9 @@
+﻿namespace Chess.Models
+{
+    abstract class Spot
+    {
+        public int X {  get; set; }
+        public int Y { get; set; }
+        public Piece? Piece { get; set; }
+    }
+}
